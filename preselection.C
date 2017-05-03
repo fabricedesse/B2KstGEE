c4205e4128cc3fbcd7c96efe9a5124eccec63df9
Fabrice Desse <fdesse@lal.in2p3.fr>
2017-05-03 19:10:59 +0200

    added preselection but too much events compared with Simone

---
+++ preselection.C
@@ -0,0 +1,85 @@
+#include "TFile.h"
+#include "TTree.h"
+#include "TTreeReader.h"
+#include "TTreeReaderValue.h"
+#include "TMath.h"
+#include <iostream>
+
+using namespace std;
+
+void MakePreselection()
+{
+  Int_t K_L0Calo_HCAL_region, Pi_L0Calo_HCAL_region, E1_L0Calo_ECAL_region, E2_L0Calo_ECAL_region;
+  Bool_t K_hasRich, Pi_hasRich, E1_hasRich, E2_hasRich, E1_hasCalo, E2_hasCalo;
+  Double_t K_PT, Pi_PT, E1_PT, E2_PT, K_TRACK_CHI2NDOF, Pi_TRACK_CHI2NDOF, E1_TRACK_CHI2NDOF, E2_TRACK_CHI2NDOF, K_TRACK_GhostProb, Pi_TRACK_GhostProb, E1_TRACK_GhostProb, E2_TRACK_GhostProb, E1_L0Calo_ECAL_xProjection, E1_L0Calo_ECAL_yProjection, E2_L0Calo_ECAL_xProjection, E2_L0Calo_ECAL_yProjection, Kst_M, B0_M23_Subst3_pi2K, B0_M012, B0_M013_Subst3_pi2K, cosThetaL, B0_HOP_M, B0_FDCHI2_OWNPV, JPs_M, B0_PV_M;
+
+  TFile *f = new TFile("../data/Raw/MC_Raw.root","read");
+  TFile newFile("../data/CreateTuple/MC_EE_candidates.root","RECREATE");
+
+  // Bd2KstGEE
+  TTree *oldTree = (TTree*)f->Get("Bd2KstGEE");
+  TTree *newTree = oldTree->CloneTree(0);
+
+  oldTree->SetBranchAddress("K_L0Calo_HCAL_region", &K_L0Calo_HCAL_region);
+  oldTree->SetBranchAddress("Pi_L0Calo_HCAL_region", &Pi_L0Calo_HCAL_region);
+  oldTree->SetBranchAddress("E1_L0Calo_ECAL_region", &E1_L0Calo_ECAL_region);
+  oldTree->SetBranchAddress("E2_L0Calo_ECAL_region", &E2_L0Calo_ECAL_region);
+  oldTree->SetBranchAddress("K_hasRich", &K_hasRich);
+  oldTree->SetBranchAddress("Pi_hasRich", &Pi_hasRich);
+  oldTree->SetBranchAddress("E1_hasRich", &E1_hasRich);
+  oldTree->SetBranchAddress("E2_hasRich", &E2_hasRich);
+  oldTree->SetBranchAddress("E1_hasCalo", &E1_hasCalo);
+  oldTree->SetBranchAddress("E2_hasCalo", &E2_hasCalo);
+  oldTree->SetBranchAddress("K_PT", &K_PT);
+  oldTree->SetBranchAddress("Pi_PT", &Pi_PT);
+  oldTree->SetBranchAddress("E1_PT", &E1_PT);
+  oldTree->SetBranchAddress("E2_PT", &E2_PT);
+  oldTree->SetBranchAddress("K_TRACK_CHI2NDOF", &K_TRACK_CHI2NDOF);
+  oldTree->SetBranchAddress("Pi_TRACK_CHI2NDOF", &Pi_TRACK_CHI2NDOF);
+  oldTree->SetBranchAddress("E1_TRACK_CHI2NDOF", &E1_TRACK_CHI2NDOF);
+  oldTree->SetBranchAddress("E2_TRACK_CHI2NDOF", &E2_TRACK_CHI2NDOF);
+  oldTree->SetBranchAddress("K_TRACK_GhostProb", &K_TRACK_GhostProb);
+  oldTree->SetBranchAddress("Pi_TRACK_GhostProb", &Pi_TRACK_GhostProb);
+  oldTree->SetBranchAddress("E1_TRACK_GhostProb", &E1_TRACK_GhostProb);
+  oldTree->SetBranchAddress("E2_TRACK_GhostProb", &E2_TRACK_GhostProb);
+  oldTree->SetBranchAddress("E1_L0Calo_ECAL_xProjection", &E1_L0Calo_ECAL_xProjection);
+  oldTree->SetBranchAddress("E1_L0Calo_ECAL_yProjection", &E1_L0Calo_ECAL_yProjection);
+  oldTree->SetBranchAddress("E2_L0Calo_ECAL_xProjection", &E2_L0Calo_ECAL_xProjection);
+  oldTree->SetBranchAddress("E2_L0Calo_ECAL_yProjection", &E2_L0Calo_ECAL_yProjection);
+  oldTree->SetBranchAddress("Kst_M", &Kst_M);
+  oldTree->SetBranchAddress("B0_M23_Subst3_pi2K", &B0_M23_Subst3_pi2K);
+  oldTree->SetBranchAddress("B0_M012", &B0_M012);
+  oldTree->SetBranchAddress("B0_M013_Subst3_pi2K", &B0_M013_Subst3_pi2K);
+  oldTree->SetBranchAddress("cosThetaL", &cosThetaL);
+  oldTree->SetBranchAddress("B0_HOP_M", &B0_HOP_M);
+  oldTree->SetBranchAddress("B0_FDCHI2_OWNPV", &B0_FDCHI2_OWNPV);
+  oldTree->SetBranchAddress("JPs_M", &JPs_M);
+  oldTree->SetBranchAddress("B0_PV_M", &B0_PV_M);
+
+  int nentries = (int)oldTree->GetEntries();
+
+  for(int i=0; i<nentries; i++)
+  {
+    oldTree->GetEntry(i);
+
+    Bool_t IsCaloRegion = TMath::Min(K_L0Calo_HCAL_region, Pi_L0Calo_HCAL_region) >= 0 && TMath::Min(E1_L0Calo_ECAL_region, E2_L0Calo_ECAL_region) >= 0;
+    Bool_t IsCaloRich = K_hasRich && Pi_hasRich && E1_hasRich && E2_hasRich && E1_hasCalo && E2_hasCalo;
+    Bool_t IsPt = K_PT > 250 && Pi_PT > 250 && TMath::Min(E1_PT, E2_PT) > 500;
+    Bool_t IsTrack = K_TRACK_CHI2NDOF < 3 && Pi_TRACK_CHI2NDOF < 3 && E1_TRACK_CHI2NDOF < 3 && E2_TRACK_CHI2NDOF < 3;
+    Bool_t IsNotGhost = K_TRACK_GhostProb < 0.4 && Pi_TRACK_GhostProb < 0.4 && E1_TRACK_GhostProb < 0.4 && E2_TRACK_GhostProb < 0.4;
+    Bool_t IsCaloProj = !((TMath::Abs(E1_L0Calo_ECAL_xProjection) < 363.6 && TMath::Abs(E1_L0Calo_ECAL_yProjection) < 282.6) || (TMath::Abs(E2_L0Calo_ECAL_xProjection) < 363.6 && TMath::Abs(E2_L0Calo_ECAL_yProjection) < 282.6));
+    Bool_t IsMass = TMath::Abs(Kst_M - 895.94) < 100 && B0_M23_Subst3_pi2K > 1040 && TMath::Max(B0_M012, B0_M013_Subst3_pi2K) < 5100;
+    Bool_t IsCosThetaL = TMath::Abs(cosThetaL) < 0.8;
+    Bool_t IsHOP = B0_HOP_M > 3350 + 135 * TMath::Log(B0_FDCHI2_OWNPV);
+    Bool_t IsJPs_M = TMath::Power(JPs_M/1000.,2) < TMath::Power(4./1000.,2);
+    Bool_t IsB0_PV_M = B0_PV_M > 4500. && B0_PV_M < 6200.;
+
+    if( IsCaloRegion && IsCaloRich && IsPt && IsTrack && IsNotGhost && IsCaloProj && IsJPs_M && IsMass && IsCosThetaL && IsHOP && IsB0_PV_M )
+    {
+      newTree->Fill();
+    }
+  }
+  newTree->Write();
+  cout << newTree->GetEntries() << endl;
+  newFile.Write();
+}
---
+++ preselection_C.d
@@ -0,0 +1,87 @@
+
+# DO NOT DELETE
+
+./preselection_C.so: /home/fdesse/root-build/include/TFile.h
+./preselection_C.so: /usr/include/c++/5/atomic
+./preselection_C.so: /home/fdesse/root-build/include/TDirectoryFile.h
+./preselection_C.so: /home/fdesse/root-build/include/TDirectory.h
+./preselection_C.so: /home/fdesse/root-build/include/TBuffer.h
+./preselection_C.so: /home/fdesse/root-build/include/TObject.h
+./preselection_C.so: /home/fdesse/root-build/include/RConfigure.h
+./preselection_C.so: /home/fdesse/root-build/include/Rtypes.h
+./preselection_C.so: /home/fdesse/root-build/include/RtypesCore.h
+./preselection_C.so: /home/fdesse/root-build/include/RConfig.h
+./preselection_C.so: /home/fdesse/root-build/include/RVersion.h
+./preselection_C.so: /home/fdesse/root-build/include/DllImport.h
+./preselection_C.so: /home/fdesse/root-build/include/snprintf.h
+./preselection_C.so: /home/fdesse/root-build/include/strlcpy.h
+./preselection_C.so: /usr/include/c++/5/typeinfo
+./preselection_C.so: /home/fdesse/root-build/include/TGenericClassInfo.h
+./preselection_C.so: /usr/include/c++/5/vector
+./preselection_C.so: /home/fdesse/root-build/include/TSchemaHelper.h
+./preselection_C.so: /usr/include/c++/5/string
+./preselection_C.so: /home/fdesse/root-build/include/TIsAProxy.h
+./preselection_C.so: /home/fdesse/root-build/include/TVirtualIsAProxy.h
+./preselection_C.so: /home/fdesse/root-build/include/TStorage.h
+./preselection_C.so: /home/fdesse/root-build/include/TVersionCheck.h
+./preselection_C.so: /usr/include/c++/5/iosfwd
+./preselection_C.so: /home/fdesse/root-build/include/TNamed.h
+./preselection_C.so: /home/fdesse/root-build/include/TString.h
+./preselection_C.so: /home/fdesse/root-build/include/TMathBase.h
+./preselection_C.so: /usr/include/c++/5/cstdlib /usr/include/c++/5/cmath
+./preselection_C.so: /home/fdesse/root-build/include/RStringView.h
+./preselection_C.so: /home/fdesse/root-build/include/RWrap_libcpp_string_view.h
+./preselection_C.so: /usr/include/c++/5/algorithm /usr/include/c++/5/iterator
+./preselection_C.so: /usr/include/c++/5/ostream /usr/include/c++/5/iomanip
+./preselection_C.so: /usr/include/c++/5/stdexcept
+./preselection_C.so: /home/fdesse/root-build/include/libcpp_string_view.h
+./preselection_C.so: /home/fdesse/root-build/include/TList.h
+./preselection_C.so: /home/fdesse/root-build/include/TSeqCollection.h
+./preselection_C.so: /home/fdesse/root-build/include/TCollection.h
+./preselection_C.so: /home/fdesse/root-build/include/TIterator.h
+./preselection_C.so: /home/fdesse/root-build/include/TDatime.h
+./preselection_C.so: /home/fdesse/root-build/include/TUUID.h
+./preselection_C.so: /home/fdesse/root-build/include/TMap.h
+./preselection_C.so: /home/fdesse/root-build/include/THashTable.h
+./preselection_C.so: /home/fdesse/root-build/include/TUrl.h
+./preselection_C.so: /home/fdesse/root-build/include/ROOT/TRWSpinLock.hxx
+./preselection_C.so: /home/fdesse/root-build/include/ROOT/TSpinMutex.hxx
+./preselection_C.so: /usr/include/c++/5/condition_variable
+./preselection_C.so: /usr/include/c++/5/mutex
+./preselection_C.so: /home/fdesse/root-build/include/TTree.h
+./preselection_C.so: /home/fdesse/root-build/include/TBranch.h
+./preselection_C.so: /usr/include/c++/5/memory
+./preselection_C.so: /home/fdesse/root-build/include/TObjArray.h
+./preselection_C.so: /home/fdesse/root-build/include/TAttFill.h
+./preselection_C.so: /home/fdesse/root-build/include/TDataType.h
+./preselection_C.so: /home/fdesse/root-build/include/TDictionary.h
+./preselection_C.so: /home/fdesse/root-build/include/ESTLType.h
+./preselection_C.so: /home/fdesse/root-build/include/TAttLine.h
+./preselection_C.so: /home/fdesse/root-build/include/TAttMarker.h
+./preselection_C.so: /home/fdesse/root-build/include/TArrayD.h
+./preselection_C.so: /home/fdesse/root-build/include/TArray.h
+./preselection_C.so: /home/fdesse/root-build/include/TArrayI.h
+./preselection_C.so: /home/fdesse/root-build/include/TClass.h
+./preselection_C.so: /home/fdesse/root-build/include/TObjString.h
+./preselection_C.so: /usr/include/c++/5/map /usr/include/c++/5/set
+./preselection_C.so: /usr/include/c++/5/unordered_set
+./preselection_C.so: /home/fdesse/root-build/include/ThreadLocalStorage.h
+./preselection_C.so: /home/fdesse/root-build/include/TVirtualTreePlayer.h
+./preselection_C.so: /usr/include/c++/5/limits
+./preselection_C.so: /home/fdesse/root-build/include/TTreeReader.h
+./preselection_C.so: /home/fdesse/root-build/include/TTreeReaderUtils.h
+./preselection_C.so: /home/fdesse/root-build/include/TBranchProxyDirector.h
+./preselection_C.so: /usr/include/c++/5/list
+./preselection_C.so: /home/fdesse/root-build/include/TBranchProxy.h
+./preselection_C.so: /home/fdesse/root-build/include/TClonesArray.h
+./preselection_C.so: /home/fdesse/root-build/include/Riostream.h
+./preselection_C.so: /usr/include/c++/5/fstream /usr/include/c++/5/iostream
+./preselection_C.so: /home/fdesse/root-build/include/TError.h
+./preselection_C.so: /home/fdesse/root-build/include/TVirtualCollectionProxy.h
+./preselection_C.so: /home/fdesse/root-build/include/TClassRef.h
+./preselection_C.so: /home/fdesse/root-build/include/TRef.h
+./preselection_C.so: /home/fdesse/root-build/include/TTreeReaderValue.h
+./preselection_C.so: /usr/include/c++/5/type_traits /usr/include/c++/5/deque
+./preselection_C.so: /home/fdesse/root-build/include/TMath.h
+./preselection_C.so: /home/fdesse/root-build/include/RVersion.h /home/fdesse/root-build/include/RConfig.h /home/fdesse/root-build/include/TClass.h /home/fdesse/root-build/include/TDictAttributeMap.h /home/fdesse/root-build/include/TInterpreter.h /home/fdesse/root-build/include/TROOT.h /home/fdesse/root-build/include/TBuffer.h /home/fdesse/root-build/include/TMemberInspector.h /home/fdesse/root-build/include/TError.h /home/fdesse/root-build/include/RtypesImp.h /home/fdesse/root-build/include/TIsAProxy.h /home/fdesse/root-build/include/TFileMergeInfo.h /home/fdesse/root-build/include/TCollectionProxyInfo.h /home/fdesse/root-build/bin/rootcling
+preselection_C__ROOTBUILDVERSION= 6.09/03
